52f297f18f3ee9f4601d57295c71669647a12614
teqlawaski <mrys@ippt.pan.pl>
2026-01-13 12:44:33 +0100

    C01/ex04 finished

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -1,47 +1,72 @@
 #include <fstream>
-#include <string>
 #include <iostream>
+#include <string>
 
-int	main() {
+static std::string replaceAll(const std::string &input,
+							  const std::string &s1,
+							  const std::string &s2)
+{
+	std::string out;
+	std::string::size_type pos = 0;
+	std::string::size_type found;
 
-	std::string file = "abc";
-	std::string s1 = "XX";
-	std::string	s2 = "co tam slychac";
-	std::string	result;
+	while ((found = input.find(s1, pos)) != std::string::npos)
+	{
+		out.append(input, pos, found - pos);
+		out.append(s2);
+		pos = found + s1.length();
+	}
+	out.append(input, pos, std::string::npos);
+	return out;
+}
 
-	std::ifstream in((file).c_str());
-	if (!in) {
-		std::cerr << "Failed to open input file\n";
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>\n";
 		return 1;
 	}
-	std::ofstream out((file + ".replace").c_str());
-	if (!out) {
-		std::cerr << "Failed to open output file\n";
+
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	if (s1.empty())
+	{
+		std::cerr << "Error: s1 must not be empty\n";
 		return 1;
 	}
 
-	std::string line;
-	while (std::getline(in, line)) {
-		std::cout << line << '\n';
-		std::size_t	pos = 0;
-		
-		while (true) {
-			std::size_t found = line.find(s1, pos);
-
-			if (found == std::string::npos) {
-				result += line.substr(pos);
-				break;
-			}
-			result += line.substr(pos, found - pos);
-			result += s2;
-			pos = found + s1.length();
-		}
-		std::cout << result << std::endl;
-		out << result << "\n";
-		result = "";
+	std::ifstream in(filename.c_str(), std::ios::in | std::ios::binary);
+	if (!in)
+	{
+		std::cerr << "Error: cannot open input file\n";
+		return 1;
 	}
 
+	std::string content;
+	{
+		std::string tmp;
+		char ch;
+		while (in.get(ch))
+			content += ch;
+	}
 
-	
+	std::ofstream out((filename + ".replace").c_str(),
+					  std::ios::out | std::ios::binary);
+	if (!out)
+	{
+		std::cerr << "Error: cannot open output file\n";
+		return 1;
+	}
+
+	out << replaceAll(content, s1, s2);
+	if (!out)
+	{
+		std::cerr << "Error: write failed\n";
+		return 1;
+	}
+
 	return 0;
 }
