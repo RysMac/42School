b460b0f7999541d67cf9c7474c108a68f216bef5
MR <maciej_rys@o2.pl>
2026-01-14 16:11:51 +0100

    adding C02/ex00

---
+++ C02/ex00/Fixed.cpp
@@ -0,0 +1,35 @@
+#include "Fixed.hpp"
+#include <string>
+#include <iostream>
+
+const int Fixed::_nbits = 8;
+
+Fixed::Fixed() : _integer(0) {
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed& other) : _integer(other._integer) {
+	std::cout << "Copy constructor called\n";
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignment operator called\n";
+	if ( this != &other ) {
+		_integer = other.getRawBits();
+	}
+	return *this;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called\n";
+}
+
+int		Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called\n";
+	return this->_integer;
+}
+
+void	Fixed::setRawBits( int const raw ) {
+	std::cout << "setRawBits member function called\n";
+	this->_integer = raw;
+}
